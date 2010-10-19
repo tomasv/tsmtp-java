1b90680f344206491060f2e4926e8848c3254bb5
tomasv <t.varneckas@gmail.com>
2010-10-19 22:54:28 +0300

    Initial commit.

---
+++ SmtpServer.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+class SmtpServer {
+	private int port = 3490;
+
+	public SmtpServer(int port) {
+		this.port = port;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+	}
+}
